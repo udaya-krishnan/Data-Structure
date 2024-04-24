25b94e590d49dcb593782d321161f8570db66f8c
udaya-krishnan <150499864+udaya-krishnan@users.noreply.github.com>
2024-04-24 12:46:51 +0530

    merge Sort

---
+++ Sort/mergeSort.js
@@ -0,0 +1,38 @@
+
+
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let index = Math.floor(arr.length / 2);
+
+  let left = arr.slice(0, index);
+
+  let right = arr.slice(index);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+
+
+
+
+function merge(left, right) {
+  let sortArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortArr.push(left.shift());
+    } else {
+      sortArr.push(right.shift());
+    }
+  }
+
+  return [...sortArr, ...left, ...right];
+}
+
+let arr = [9, 0, 4, 2, 8, 6];
+
+let result = mergeSort(arr);
+
+console.log(result);//[0,2,4,6,8,9]
